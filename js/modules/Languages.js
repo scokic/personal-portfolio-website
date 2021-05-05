32c6afccb07902bb70e51362438600ee8a9246a9
Strahinja Cokic <55125319+scokic@users.noreply.github.com>
2021-05-05 17:18:01 +0200

    Translation to English language ,restructured generation of html content from JS, and minor CSS fixes
    - Added language translation to English language! Text content is now pulled from separate JSON files!
    - Restructured generation of home page content. All data is now pulled from JS file instead of html.
    - Minor CSS improvements

---
+++ js/modules/Languages.js
@@ -0,0 +1,51 @@
+// Language toggle
+
+const body = document.getElementsByTagName("BODY")[0];
+const langButton = document.querySelector("#language-toggle-container");
+
+const english = document.getElementById("english");
+const serbian = document.getElementById("serbian");
+
+langButton.addEventListener("click", () => {
+  body.classList.toggle("en");
+});
+
+// Language json fetch and UI replacing
+
+// Sometimes: napraviti čuvanje varijable u lokalnom storage-u
+// Sometimes: napraviti čitanje informacije iz lokalnog storage-a
+
+// dohvatanje dugmića za promenu jezika
+
+const serbianLanguage = document.getElementById("serbian");
+const englishLanguage = document.getElementById("english");
+
+// dohvatanje prevoda na klik
+
+englishLanguage.addEventListener("click", renderEnglishContent);
+serbianLanguage.addEventListener("click", renderSerbianContent);
+
+function renderEnglishContent() {
+  renderContent("English");
+}
+
+function renderSerbianContent() {
+  renderContent("Serbian");
+}
+
+function renderContent(language) {
+  console.time("renderNewContent");
+  language = fetch(`js/translation/${language}.json`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((language) => {
+      for (let [key, value] of Object.entries(language)) {
+        let objectProperty = document.getElementById(`${key}`);
+        objectProperty.textContent = `${value}`;
+      }
+    });
+  console.timeEnd("renderNewContent");
+}
+
+renderSerbianContent();
